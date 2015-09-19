97cf0b017c2ea192e98bf1d01337196899f1ff73
Sean West <sean@unf.int>
2015-09-18 23:43:41 -0700

    dancey foo foo

--- js/app.js
+++ js/app.js
@@ -26,11 +26,14 @@ function compileTemplate(tpl, vars) {
 }
 
 function generateReport(header, rows) {
+    rows.pop();
+
     $report.empty();
  
     var reportName =  $('#reportNameInput').val(),
-    reportAddress =  $('#reportAddressInput').val(),
-    $reportHeader = $('#reportHeader');
+        reportAddress =  $('#reportAddressInput').val(),
+        $reportHeader = $('#reportHeader');
+
     //
     // process rows, create data to pass to templates
     //
@@ -53,6 +56,14 @@ function generateReport(header, rows) {
         "averageArea": 0,
         "averagePSF":0
     };
+    var summary = {}, 
+        summarytots = { 
+            total: {
+                price: 0,
+                num_sold: 0,
+                area: 0
+            }
+        };
 
     rows.forEach(function(row) {
         [ 'List Price', 'Sold Price', 'Sold Price per SqFt', 'TotFlArea' ].forEach(function(column) {
@@ -83,18 +94,69 @@ function generateReport(header, rows) {
         }
     });
 
-    for( var row in rows) {
+    for (var row in rows) {
         var soldDate = rows[row]['Sold Date'];
         if (soldDate) {
-            rows[row]['Sold Date'] = moment(soldDate, 'M/D/YYYY').format('MMM D, YYYY');
+            soldDate = moment(soldDate, 'M/D/YYYY');
+            rows[row]['Sold Date'] = soldDate.format('MMM D, YYYY');
         }
 
-        if(rows[row].Status == "S") {
+        if (rows[row].Status == "S") {
             sold.rows.push(rows[row]);
             sold.totalListPrice = rows[row]["List Price"] + sold.totalListPrice;
             sold.totalSalePrice = rows[row]["Sold Price"] + sold.totalSalePrice;
             sold.totalArea = rows[row].TotFlArea + sold.totalArea;
             
+            var ssd = soldDate.format('MMM - YY'),
+                s = summary[ssd];
+
+            if (!s) {
+                s = summary[ssd] = {
+                    total: {
+                        price: 0,
+                        area: 0,
+                        num_sold: 0
+                    }
+                };
+            }
+
+            var br = rows[row]['Tot BR'],
+                b = s[br];
+
+            if (!b) {
+                b = s[br] = {
+                    price: 0,
+                    area: 0,
+                    num_sold: 0
+                };
+            }
+
+            ++b.num_sold;
+            b.price += rows[row]['Sold Price'];
+            b.area += rows[row]['TotFlArea'];
+
+            ++s.total.num_sold;
+            s.total.price += rows[row]['Sold Price'];
+            s.total.area += rows[row]['TotFlArea'];
+
+            var br = rows[row]['Tot BR'],
+                brt = summarytots[br];
+
+            if (!brt) {
+                brt = summarytots[br] = {
+                    price: 0,
+                    area: 0,
+                    num_sold: 0
+                };
+            }
+
+            ++brt.num_sold;
+            brt.price += rows[row]['Sold Price'];
+            brt.area += rows[row]['TotFlArea'];
+
+            ++summarytots.total.num_sold;
+            summarytots.total.price += rows[row]['Sold Price'];
+            summarytots.total.area += rows[row]['TotFlArea'];
         } else {
             listings.rows.push(rows[row]);
             listings.totalListPrice = rows[row]["List Price"] + listings.totalListPrice;
@@ -114,20 +176,43 @@ function generateReport(header, rows) {
         [ 'List Price', 'Sold Price', 'Sold Price per SqFt' ].forEach(function(column) {
             rows[row][column] = rows[row][column].formatMoney(0);
         });
-
     }
 
-    console.log(listings.rows);
+    for (var month in summary) {
+        for (var nbr in summary[month]) {
+            var br = summary[month][nbr];
+            br.psf = (br.price / br.area).formatMoney(0);
+        }
+    }
+
+    for (var nbr in summarytots) {
+        var br = summarytots[nbr];
+        br.psf = (br.price / br.area).formatMoney(0);
+    }
+
+    console.log(listings.rows, summarytots);
 
     //
     // compile templates and append to report
     //
+    $report.append(compileTemplate('reportHeading',{ reportName: reportName, reportAddress: reportAddress }));
+
     $report.append(compileTemplate('salesHeading', { title: 'SALES' }));
-    $report.append(compileTemplate('salesDump', { header: header, rows: sold.rows, totalListPrice: sold.totalListPrice.formatMoney(0) , totalSalePrice: sold.totalSalePrice.formatMoney(0), totalArea: sold.totalArea.formatMoney(0), averageListPrice: sold.averageListPrice, averageSalePrice: sold.averageSalePrice, averageArea: sold.averageArea, averagePSF: sold.averagePSF}));
+    $report.append(compileTemplate('salesDump', { 
+        header: header, rows: sold.rows, totalListPrice: sold.totalListPrice.formatMoney(0), 
+        totalSalePrice: sold.totalSalePrice.formatMoney(0), totalArea: sold.totalArea.formatMoney(0), 
+        averageListPrice: sold.averageListPrice, averageSalePrice: sold.averageSalePrice, 
+        averageArea: sold.averageArea, averagePSF: sold.averagePSF
+    }));
 
     $report.append(compileTemplate('listingsHeading', { title: 'LISTINGS' }));
-    $report.append(compileTemplate('listingsDump', { header: header, rows: listings.rows, totalListPrice: listings.totalListPrice.formatMoney(0) , totalArea: listings.totalArea.formatMoney(0), averageListPrice: listings.averageListPrice, averageArea: listings.averageArea, averagePSF: listings.averagePSF }));
-    $reportHeader.append(compileTemplate('reportHeader',{reportName: reportName, reportAddress: reportAddress}));
+    $report.append(compileTemplate('listingsDump', {
+        header: header, rows: listings.rows, totalListPrice: listings.totalListPrice.formatMoney(0), 
+        totalArea: listings.totalArea.formatMoney(0), averageListPrice: listings.averageListPrice, 
+        averageArea: listings.averageArea, averagePSF: listings.averagePSF 
+    }));
+
+    $report.append(compileTemplate('summary', { summary: summary, summarytots: summarytots }));
 }
 
 Number.prototype.formatMoney = function(c, d, t){
